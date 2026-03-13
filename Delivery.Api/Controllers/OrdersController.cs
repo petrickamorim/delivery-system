51de655f59d003c273d3c4b87edc4e40656f663a
petrickamorim <gamehunter200513@gmail.com>
2026-03-12 22:52:31 -0300

    feat: add controllers and repositories

---
+++ Delivery.Api/Controllers/OrdersController.cs
@@ -0,0 +1,25 @@
+﻿using Delivery.Application.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Delivery.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public OrdersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder([FromBody] CreateOrderCommand command)
+        {
+            var orderId = await _mediator.Send(command);
+
+            return Ok(orderId);
+        }
+    }
+}
---
+++ Delivery.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,40 @@
+﻿using Delivery.Domain.Entities;
+using Delivery.Domain.Interfaces;
+
+namespace Delivery.Infrastructure.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly List<Order> _orders = new();
+
+        public Task AddAsync(Order order)
+        {
+            _orders.Add(order);
+            return Task.CompletedTask;
+        }
+
+        public Task<IEnumerable<Order>> GetAllAsync()
+        {
+            return Task.FromResult(_orders.AsEnumerable());
+        }
+
+        public Task<Order?> GetByIdAsync(Guid id)
+        {
+            var order = _orders.FirstOrDefault(x => x.Id == id);
+            return Task.FromResult(order);
+        }
+
+        public Task UpdateAsync(Order order)
+        {
+            var existingOrder = _orders.FirstOrDefault(x => x.Id == order.Id);
+
+            if (existingOrder != null)
+            {
+                _orders.Remove(existingOrder);
+                _orders.Add(order);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
